794cef3d7b2a08341cbf852478630973271415cf
Raincal <cyj94228@gmail.com>
2015-05-12 03:45:29 +0800

    index done

--- app.js
+++ app.js
@@ -30,8 +30,71 @@ app.use('/', routes);
 app.use('/users', users);
 app.use('/hello', hello);
 
+app.get('/u/:user',routes);
+app.post('/post',routes);
+app.get('/reg',routes);
+app.post('/doReg',routes);
+app.get('/login',routes);
+app.post('/doLogin',routes);
+app.get('/logout',routes);
+
+
+/*app.all('/user/:username',function(req,res,next){
+  console.log('all methods captured');
+  next();
+})
 app.get('/user/:username',function(req,res){
   res.send('user: ' + req.params.username);
+});*/
+
+//检测用户名是否存在在
+var users = {
+  'yujia' : {
+    name : 'Raincal',
+    website : 'www.cyj228.com'
+  }
+};
+app.all('/user/:username',function(req,res,next){
+  if(users[req.params.username]){
+    next();
+  }else{
+    next(new Error(req.params.username + ' dose not exit.'));
+  }
+});
+app.get('/user/:username',function(req,res){
+  res.send(JSON.stringify(users[req.params.username]));
+});
+app.put('/user/:username',function(req,res){
+  res.end('Done.');
+});
+
+//片段视图(partials)
+app.get('/list',function(req,res){
+  res.render('list',{
+    title : 'List',
+    items : [1994,'Raincal','express','Nodejs']
+  });
+});
+
+//视图助手
+var util= require('util');
+/*express3.x
+  app.locals({
+  inspect : function(obj){
+    return util.inspect(obj,true);
+  }
+});*/
+app.locals.inspect = function(obj){
+  return util.inspect(obj,true);
+};
+app.use(function(req,res,next){
+  res.locals.headers = req.headers;
+  next();
+});
+app.get('/helper',function(req,res){
+  res.render('helper',{
+    title : 'Helpers'
+  });
 });
 
 // catch 404 and forward to error handler
