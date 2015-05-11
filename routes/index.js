3ca3c5dbb5cbf45bacbf91592cc5dceffce2714d
Raincal <cyj94228@gmail.com>
2015-05-12 02:44:30 +0800

    路由规划

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,26 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+router.get('/u/:user',function(req,res){
+  res.send('用户名');
+});
+router.use('/post',function(req,res){
+  res.send('post');
+});
+router.get('/reg',function(req,res){
+  res.send('reg');
+});
+router.use('/doReg',function(req,res){
+  res.send('doReg');
+});
+router.get('/login',function(req,res){
+  res.send('login');
+});
+router.use('/doLogin',function(req,res){
+  res.send('doLogin');
+});
+router.get('/logout',function(req,res){
+  res.send('logout');
+});
 
 module.exports = router;
