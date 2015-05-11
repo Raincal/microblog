9187cad2ab1d6fce3f1b5ea8bee799587c82a293
Raincal <cyj94228@gmail.com>
2015-05-12 05:29:41 +0800

    mongodb

---
+++ models/db.js
@@ -0,0 +1,9 @@
+/**
+ * Created by raincal on 15/5/12.
+ */
+var settings = require('../settings');
+var Db = require('mongodb').Db;
+var Connection = require('mongodb').Connection;
+var Server = require('mongodb').Server;
+
+module.exports = new Db(settings.db,new Server(settings.host,Connection.DEFAULT_PORT,{}),{safe:true});
